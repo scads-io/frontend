0b1d38e956fad51d5641aaa2bf5ea0b6c7f210ac
logan <list12344@gmail.com>
2023-12-18 20:00:28 +0200

    Update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,33 +1,61 @@
 import React from 'react'
-import { Link } from '@scads/uikit'
 
-const Footer = ({ theme, className }) => {
+const socialIcons = [
+  {
+    id: "i1",
+    icon: "/images/home/logo-x.webp",
+    redirect: "https://twitter.com/Scads_io",
+  },
+  {
+    id: "i2",
+    icon: "/images/home/telegram_logo.svg",
+    redirect: "https://t.me/scads_io",
+  },
+  {
+    id: "i3",
+    icon: "/images/home/instagram_logo.svg",
+  },
+  {
+    id: "i4",
+    icon: "/images/home/discord_logo.svg",
+  },
+  {
+    id: "i5",
+    icon: "/images/home/github_logo.svg",
+    redirect: "https://github.com/scads-io",
+  },
+  {
+    id: "i6",
+    icon: "/images/home/logo-medium.svg",
+  },
+];
+
+const Footer = (props) => {
+  const { theme, className } = props
 
   const year = new Date().getFullYear()
 
   return (
     <div
       className={`flex flex-col justify-center items-center pb-10 lg:pb-10 gap-6 ${className}`}
-      >
-      <div className="flex gap-5">
-        <Link href='https://twitter.com/home' external>
-          <img src='/images/home/twitter_logo 1.svg' className={`${theme.isDark && "invert"}`} alt="" />
-        </Link>
-        <Link href='https://web.telegram.org/z/' external>
-          <img src='/images/home/telegram_logo 1.svg' className={`${theme.isDark && "invert"}`} alt="" />
-        </Link>
-        <Link href='https://www.facebook.com/' external>
-          <img src='/images/home/facebook_logo 1.svg' className={`${theme.isDark && "invert"}`} alt="" />
-        </Link>
-        <Link href='https://www.instagram.com/' external>
-          <img src='/images/home/instagram_logo 1.svg' className={`${theme.isDark && "invert"}`} alt="" />
-        </Link>
-        <Link href='https://discord.com/' external>
-          <img src='/images/home/discord_logo 1.svg' className={`${theme.isDark && "invert"}`} alt="" />
-        </Link>
-        <Link href='https://github.com/' external>
-          <img src='/images/home/github_logo 1.svg' className={`${theme.isDark && "invert"}`} alt="" />
-        </Link>
+    >
+      <div className="flex items-center gap-5">
+        {socialIcons.map((item) => (
+          <a
+            href={item.redirect}
+            target="_blank"
+            rel="noreferrer"
+            className="cursor-pointer"
+            key={item.id}
+          >
+            <img
+              src={item.icon}
+              className={`${theme.isDark && "invert"}`}
+              alt="social icon"
+              width={28}
+            />
+          </a>
+        ))}
       </div>
       <p
         className={`font-['Poppins'] text-lg text-center flex gap-1 items-center ${
@@ -43,7 +71,7 @@ const Footer = ({ theme, className }) => {
         />
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
