af791af4f91e8bd3c9d2187bd5ce667958c34afa
logan <list12344@gmail.com>
2024-05-12 19:08:37 +0300

    Update wagmi.ts

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -22,9 +22,12 @@ export const injectedConnector = new InjectedConnector({
 export const walletConnectConnector = new WalletConnectConnector({
   chains,
   options: {
-    /* projectId: '74251247fff43babdb46edbab5b2a868', */
-    projectId: '0eb93e1311bacfc188ea2df6c42d02fe', /* test */
-    showQrModal: true
+    projectId: process.env.NODE_ENV !== 'production' ?
+      '74251247fff43babdb46edbab5b2a868' : '0eb93e1311bacfc188ea2df6c42d02fe' ,
+    showQrModal: true,
+    qrModalOptions: {
+      themeMode: 'dark',
+    }
   },
 })
 
